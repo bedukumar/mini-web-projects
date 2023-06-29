00932cfc7b64380bf60568e837a6a965c0bceafe
bedukumar <99368814+bedukumar@users.noreply.github.com>
2023-06-30 02:01:15 +0530

    Add files via upload
    this projects utilizes how to utilize mediaDevices Under Browsers Navigator Api.in this project i have utilized it for Screen Recording.

---
+++ screenrecorder/recorder.js
@@ -0,0 +1,23 @@
+const start = async () => {
+  const stream = await navigator.mediaDevices.getDisplayMedia({
+    video: {
+      mediaSource: "screen",
+    },
+  });
+  const data = [];
+
+  const mediaRecorder = new MediaRecorder(stream);
+  mediaRecorder.ondataavailable = (e) => {
+    data.push(e.data);
+  };
+  mediaRecorder.start();
+  mediaRecorder.onstop = (e) => {
+    document.querySelector("video").src = URL.createObjectURL(
+      new Blob(data, {
+        type: data[0].type,
+      })
+    );
+  };
+};
+
+start();
